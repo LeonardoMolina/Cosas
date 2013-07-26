5ddb4c3ae618bb68f78a4f7f8f75472b396aaa8d
LeonardoMolina <molinaleonardod@gmail.com>
2013-07-25 21:52:59 -0300

    Agregando clase personaje

---
+++ src/com/leo/test/cosa/player/Personaje.java
@@ -0,0 +1,47 @@
+package com.leo.test.cosa.player;
+
+public class Personaje {
+	
+	
+	// Atributos basicos
+	private String nombre = "";
+	
+	// Stats ofensivos
+	private int minAttackDamage;
+	private int maxAttackDamage;
+	private int critPercent;
+	private int criticalDamage;
+	private int abilityPower;
+	private int magicPenetration;
+	private int armorPenetration;
+	
+	// Stats pasivos
+	private int hp;
+	private int currentHp;
+	private int rateHp;
+	private int mana;
+	private int currentMana;
+	private int rateSp;
+	private int energy;
+	private int currentEnegery;
+	private int rateEnegergy;
+	private int rage;
+	private int currentRage;
+	private int rateRage;
+	private int speed;
+	private int attackSpeed;
+	private int range;
+	
+	// Stats defensivos
+	private int armor;
+	private int magicResistance;
+	private int dodge;
+	private int block;
+	private int blockWithShield;
+	private int parry;
+	
+	
+	
+	private String tipo;
+
+}
